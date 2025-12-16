288bafc1aa00652f2308b832a75b1ac0a103ece7
tnshka18 <gargtanishka18@gmail.com>
2025-12-17 01:19:11 +0530

    hero

--- src/components/ui/landing/Hero.tsx
+++ src/components/ui/landing/Hero.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 function Hero() {
-  return (
-    <div>
-      Hero
-    </div>
-  )
+  return <section className="relative h-screen flex items-center overflow-hidden pt-20">
+    <div className="absolute inset-0 bg-linear-to-br from-background via-muted/5 to-primary/5">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)] bg-size-[4rem_4rem] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] opacity-20"></div>
+      </div>
+
+     <div className="absolute top-20 left-1/4 w-72 h-72 bg-gradient-to-r from-primary/20 to-primary/10 rounded-full blur-3xl" />
+      <div className="absolute bottom-20 right-1/4 w-96 h-96 bg-linear-to-r from-primary/15 to-primary/5 rounded-full blur-3xl" />
+
+  </section>;
 }
 
-export default Hero
+export default Hero 
 
+ 
